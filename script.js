3c07661edca899f151853d4a11903055e3fc9bae
Roziyahon Bohovodinova <bohovodinovaroziahon@gmail.com>
2024-12-25 20:40:36 +0500

    localdataBeta

--- script.js
+++ script.js
@@ -1,11 +1,61 @@
 const productsList = document.querySelector('.products-items') // все товары
+
 const formZakaz = document.getElementsByClassName('formZakaz')[0] // окно формы
 const closeButton = document.getElementById('closeButton') // кнопка закрытия
 const orderProductName = document.getElementById('orderProductName') // для названия товара
 
 const changeValuta = document.getElementsByClassName('perValuta')[0]
-
 const prices = document.getElementsByClassName('products-items-price')
+
+const data = [
+    {
+        name: "Лучшие друзья",
+        kart: "images/1.png",
+        descr: "Печенье, с которого все началось! Наше фирменное печенье с шоколадной крошкой и грецкими орехами хрустящее снаружи с достаточно толстой и липкой серединкой.",
+        pr: "20",
+        we: "2 шт./ 200 гр."
+    },
+
+    {
+        name: "Лучшие друзья",
+        kart: "images/1.png",
+        descr: "Печенье, с которого все началось! Наше фирменное печенье с шоколадной крошкой и грецкими орехами хрустящее снаружи с достаточно толстой и липкой серединкой.",
+        pr: "20",
+        we: "2 шт./ 200 гр."
+    },
+
+    {
+        name: "Лучшие друзья",
+        kart: "images/1.png",
+        descr: "Печенье, с которого все началось! Наше фирменное печенье с шоколадной крошкой и грецкими орехами хрустящее снаружи с достаточно толстой и липкой серединкой.",
+        pr: "20",
+        we: "2 шт./ 200 гр."
+    },
+
+    {
+        name: "Лучшие друзья",
+        kart: "images/1.png",
+        descr: "Печенье, с которого все началось! Наше фирменное печенье с шоколадной крошкой и грецкими орехами хрустящее снаружи с достаточно толстой и липкой серединкой.",
+        pr: "20",
+        we: "2 шт./ 200 гр."
+    },
+
+    {
+        name: "Лучшие друзья",
+        kart: "images/1.png",
+        descr: "Печенье, с которого все началось! Наше фирменное печенье с шоколадной крошкой и грецкими орехами хрустящее снаружи с достаточно толстой и липкой серединкой.",
+        pr: "20",
+        we: "2 шт./ 200 гр."
+    },
+
+    {
+        name: "Лучшие друзья",
+        kart: "images/1.png",
+        descr: "Печенье, с которого все началось! Наше фирменное печенье с шоколадной крошкой и грецкими орехами хрустящее снаружи с достаточно толстой и липкой серединкой.",
+        pr: "20",
+        we: "2 шт./ 200 гр."
+    }
+]
 
 changeValuta.addEventListener('click', function(e) {
     const currentV = e.target.innerText
@@ -54,5 +104,77 @@ window.addEventListener('click', function(event) {
     }
 })
 
+function createCookie(obj){
+    const pech = document.createElement('div')
+    pech.classList.add("products-item")//задаем ей класс
+
+    //изображение
+    const img = document.createElement('img')
+    img.src = obj.kart
+    img.alt = "somep"
+
+    const divImg = document.createElement('div')
+    divImg.classList.add("products-items-image") 
+
+    divImg.appendChild(img)
+
+    //детали
+    const details = document.createElement('div')
+    details.classList.add("products-items-details")
+
+    const prTitleJS = document.createElement('div')
+    prTitleJS.classList.add("products-items-title")
+    prTitleJS.innerText = obj.name
+
+    const prTextJS = document.createElement('div')
+    prTextJS.classList.add("products-item-text")
+    prTextJS.innerText = obj.descr
+
+    //детали, экстра
+    const prExstraJS = document.createElement('div')
+    prExstraJS.classList.add("products-item-extra")
+
+    const prExstraInfJS = document.createElement('div')
+    prExstraInfJS.classList.add("products-item-info")
+
+    const prExstraPriceJS = document.createElement('div')
+    prExstraPriceJS.classList.add("products-items-price")
+    prExstraPriceJS.innerText = obj.pr
+    // prExstraPriceJS.setAttribute('base-price')
 
 
+    const prExstraWeightJS = document.createElement('div')
+    prExstraWeightJS.classList.add("products-items-weight")
+    prExstraWeightJS.innerText = obj.we
+
+    const prBtnZ = document.createElement('button')
+    prBtnZ.classList.add("button-violet-button")
+    prBtnZ.innerText = "Заказать"
+
+    prExstraInfJS.appendChild(prExstraPriceJS)
+    prExstraInfJS.appendChild(prExstraWeightJS)
+
+    prExstraJS.appendChild(prExstraInfJS)
+    prExstraJS.appendChild(prBtnZ)
+
+    details.appendChild(prTitleJS)
+    details.appendChild(prTextJS)
+    details.appendChild(prExstraJS)
+    
+    
+
+
+    pech.appendChild(divImg)
+    pech.appendChild(details)
+
+    return pech
+}
+
+
+
+// productsList.innerHTML=''
+for (let item of data){
+    const tmpEl = createCookie(item)
+
+    productsList.appendChild(tmpEl)
+}
